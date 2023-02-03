eadf2cfe9165d2da76386cc9bed563b89690e21a
Didit Aditya <75919242+diditaditya06@users.noreply.github.com>
2023-02-03 18:16:52 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,26 @@
-var hide;
-function munculnama()
-{
-   if(hide==1) 
-   {
-    document.getElementsById("muncul").style.display="block";
-    return hide=0;
-   }
-   else
-   {
-    document.getElementsById("muncul").style.display="none";
-    return hide=1;
-   }
+'use strict';
+
+
+// Toggle class active
+const navbarNav = document.querySelector('.navbar-nav');
+// ketika hamburger menu di klik
+document.querySelector('#list-menu').onclick = () => {
+  navbarNav.classList.toggle('active');
+};
+
+// Klik di luar sidebar untuk menghilangkan nav
+const hamburger = document.querySelector('#list-menu');
+
+document.addEventListener('click', function (e) {
+if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+  navbarNav.classList.remove('active');
 }
+});
+
+const body = document.getElementById("body"); //id pada isi-data
+const input = document.getElementById("input");//id pada inputan
+const section = document.getElementById("section");
+function tekanbtn(){
+  body.style.backgroundColor = input.value //merubah warna 
+  section.style.backgroundColor = input.value 
+};
