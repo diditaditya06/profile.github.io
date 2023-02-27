cf302c231aac02ebcd8ab81871e5d5ae8495fb60
Didit Aditya <75919242+diditaditya06@users.noreply.github.com>
2023-02-27 15:19:31 +0700

    Update script.js

--- script.js
+++ script.js
@@ -24,3 +24,48 @@ function tekanbtn(){
   body.style.backgroundColor = input.value //merubah warna 
   section.style.backgroundColor = input.value 
 };
+
+
+function nama() {
+  var x = document.getElementById("hasil-nama");
+  var y = document.getElementById("bungkus-nama");
+  var z = document.getElementById("icon-nama");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    y.style.height ="85px";
+    z.src = "./asset/k.png";
+  } else {
+    x.style.display = "none";
+    y.style.height ="50px";
+    z.src = "./asset/v.png";
+  }
+}
+
+function education() {
+  var x = document.getElementById("hasil-education");
+  var y = document.getElementById("bungkus-education");
+  var z = document.getElementById("icon-education");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    y.style.height ="250px";
+    y.style.backgroundColor= "#989978";
+    z.src = "./asset/k.png";
+    
+  } else {
+    x.style.display = "none";
+    y.style.height ="50px";
+    y.style.backgroundColor= "#ecf00e";
+    z.src = "./asset/v.png";
+    
+  }
+}
+
+const count = document.getElementById("count");
+totalupdate();
+function totalupdate(){
+  fetch("https://api.countapi.xyz/hit/swapnilsprash/?amount=1")
+  .then((res)=>res.json())
+  .then((res)=>{count.innerHTML=res.value;
+  val(count,0,res.value,1000);
+  });
+} 
