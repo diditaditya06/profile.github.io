199c9c26e09cef866be740c24d510e22ede47121
Didit Aditya <75919242+diditaditya06@users.noreply.github.com>
2023-05-07 21:31:35 +0700

    Update script.js

--- script.js
+++ script.js
@@ -63,9 +63,31 @@ function education() {
 const count = document.getElementById("count");
 totalupdate();
 function totalupdate(){
-  fetch("https://api.countapi.xyz/hit/kosong/?amount=1")
+  fetch("https://api.countapi.xyz/hit/beli/?amount=0")
   .then((res)=>res.json())
   .then((res)=>{count.innerHTML=res.value;
   val(count,0,res.value,1000);
   });
 } 
+
+let mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+
+
